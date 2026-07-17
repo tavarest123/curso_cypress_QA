71642a9633cd62142fe90c75bcf1ab177fd89407
Estevão Tavares <ets09@hotmail.com>
2026-07-16 22:56:43 -0300

    alteracao no testewait

--- cypress/e2e/testewait.cy.js
+++ cypress/e2e/testewait.cy.js
@@ -1,9 +1,4 @@
-describe('teste wait', () => {
-  it('wait falha', () => {
-    cy.visit('https://example.cypress.io/commands/network-requests')
-    cy.get('.network-btn').click()
-    cy.get('.network-post').should('contain', 'Bob') // pode falhar, é instável
-  })
+
 
   it('wait sucesso', () => {
     cy.visit('https://example.cypress.io/commands/network-requests')
@@ -12,5 +7,4 @@ describe('teste wait', () => {
     cy.wait('@getComment') // só continua quando a API responder
     cy.get('.network-post').should('be.visible')
 
-})
 })
